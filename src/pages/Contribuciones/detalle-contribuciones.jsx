9ec5c2a8716d18009534388abd5d292498a1da26
NataliaLopezO <natalia.lopez.osorio@correounivalle.edu.co>
2023-06-02 12:23:39 -0500

    Se creo la pagina de introduccion para las contribuciones y su diseño inicial

---
+++ src/pages/Contribuciones/detalle-contribuciones.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { Nav_bar_perfil } from "../../components/nav-bar-perfil";
+import "../../scss/historia_style.css";
+import "../../scss/boton_toggler_style.css";
+import { Boton_back } from "../../components/boton-back";
+import { Boton_next } from "../../components/boton-next";
+
+export function Detalle() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  /**
+   * Alternar la apertura y cierre de la barra lateral.
+   *
+   * Esta función cambia el estado de la variable 'isSidebarOpen' para controlar
+   * si la barra lateral está abierta o cerrada. Si la barra lateral está abierta,
+   * la función la cerrará y viceversa.
+   */
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <>
+      <button
+        className={`boton-toggler ${isSidebarOpen ? "open" : ""}`}
+        style={{ marginLeft: isSidebarOpen ? "310px" : "0" }}
+        onClick={toggleSidebar}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      {isSidebarOpen && (
+        <div>
+          <Nav_bar_perfil />
+          <div className="sidebar-overlay" onClick={toggleSidebar}></div>
+        </div>
+      )}
+      <main
+        style={{ marginLeft: isSidebarOpen ? "310px" : "0" }}
+        className={`contenedor-historia ${isSidebarOpen ? "open" : ""}`}
+      >
+        <div className="historia-titulo">
+          <h1>Inventos y Contribuciones</h1>
+        </div>
+
+        <div
+          className="next-back"
+          style={{ display: "flex", justifyContent: "space-between" }}
+        >
+            
+          <Boton_back
+            nombre="Introducción"
+            imagen="/images/icono-contribuciones.png"
+            identificador={123}
+            href="/contribuciones"
+          />
+       
+          
+          
+        </div>
+      </main>
+    </>
+  );
+}
