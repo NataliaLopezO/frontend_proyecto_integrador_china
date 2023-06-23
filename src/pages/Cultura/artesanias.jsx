bf408039cc2c8c0c5c58666a111fad8fe0d6192d
Hernando Lopez <hernando.rincon@correounivalle.edu.co>
2023-06-23 01:30:55 -0500

    Se comienza a implementar modal para la pagina de artesanias y comenzar a agregar modelos

--- src/pages/Cultura/artesanias.jsx
+++ src/pages/Cultura/artesanias.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 import { Nav_bar_perfil } from "../../components/nav-bar-perfil";
 import { Boton_back } from "../../components/boton-back";
 import { Boton_next } from "../../components/boton-next";
+import { Modelo_dinastia } from "../../components/Modelo_dinastia";
+import { Canvas } from "react-three-fiber";
+import { Float, Text, OrbitControls } from "@react-three/drei";
 import "../../scss/artesanias_style.css";
 
 /**
@@ -24,6 +28,39 @@ export function Artesanias() {
    */
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const [showModal, setShowModal] = useState(false);
+
+  /**
+   * Abre el modal.
+   * Actualiza el estado 'showModal' a true para mostrar el modal.
+   */
+
+  const handleModalOpen = () => {
+    setShowModal(true);
+  };
+
+  /**
+   * Cierra el modal.
+   * Actualiza el estado 'showModal' a false para ocultar el modal.
+   */
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  const [showModal2, setShowModal2] = useState(false);
+
+  const handleModalOpen2 = () => {
+    setShowModal2(true);
+  };
+
+  /**
+   * Cierra el modal.
+   * Actualiza el estado 'showModal' a false para ocultar el modal.
+   */
+  const handleModalClose2 = () => {
+    setShowModal2(false);
   };
 
   return (
@@ -78,7 +115,15 @@ export function Artesanias() {
               </p>
             </div>
             <div className="cont-mod-pintura">
-              <button className="boton-pint">VER</button>
+              <Button
+                type="button"
+                className="boton-pint"
+                size="md"
+                onClick={handleModalOpen}
+                variant="primary"
+              >
+                VER
+              </Button>
             </div>
           </div>
         </div>
@@ -145,7 +190,15 @@ export function Artesanias() {
               />
             </div>
             <div className="cont-mod-cerami">
-              <button className="boton-pint">VER</button>
+              <Button
+                type="button"
+                className="boton-pint"
+                size="md"
+                onClick={handleModalOpen2}
+                variant="primary"
+              >
+                VER
+              </Button>
             </div>
           </div>
         </div>
@@ -189,6 +242,112 @@ export function Artesanias() {
           </div>
         </div>
 
+        <Modal
+          show={showModal}
+          onHide={handleModalClose}
+          centered
+          backdrop="static"
+        >
+          <Modal.Header>
+            <Modal.Title>Guerrero Chino</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div
+              className="modelo-dinastia"
+              style={{
+                width: "100%",
+                height: "500px",
+                marginBottom: "0",
+              }}
+            >
+              <Canvas
+                camera={{
+                  fov: 45,
+                  near: 0.005,
+                  far: 800,
+                  position: [300, 5, -150],
+                }}
+              >
+                {/* Controles */}
+                <OrbitControls makeDefault />
+
+                {/* Luces */}
+                <directionalLight position={[5, 1, 6]} intensity={0.7} />
+                <ambientLight intensity={0.5} />
+
+                <Modelo_dinastia
+                  position={[-10, -50, 10]}
+                  scale={20}
+                ></Modelo_dinastia>
+              </Canvas>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              type="submit"
+              variant="secondary"
+              onClick={() => {
+                handleModalClose();
+              }}
+            >
+              Cerrar
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
+        <Modal
+          show={showModal2}
+          onHide={handleModalClose2}
+          centered
+          backdrop="static"
+        >
+          <Modal.Header>
+            <Modal.Title>Guerrero Chino</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div
+              className="modelo-dinastia"
+              style={{
+                width: "100%",
+                height: "500px",
+                marginBottom: "0",
+              }}
+            >
+              <Canvas
+                camera={{
+                  fov: 45,
+                  near: 0.005,
+                  far: 800,
+                  position: [300, 5, -150],
+                }}
+              >
+                {/* Controles */}
+                <OrbitControls makeDefault />
+
+                {/* Luces */}
+                <directionalLight position={[5, 1, 6]} intensity={0.7} />
+                <ambientLight intensity={0.5} />
+
+                <Modelo_dinastia
+                  position={[-10, -50, 10]}
+                  scale={20}
+                ></Modelo_dinastia>
+              </Canvas>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              type="submit"
+              variant="secondary"
+              onClick={() => {
+                handleModalClose2();
+              }}
+            >
+              Cerrar
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
         <div
           className="next-back"
           style={{ display: "flex", justifyContent: "space-between" }}
@@ -203,10 +362,9 @@ export function Artesanias() {
           <Boton_next
             nombre="Festividades"
             imagen="/images/festividades-icono.png"
-            identificador='cultura'
+            identificador="cultura"
             href="/festividades"
-            categoria= 'artesanias'
-            
+            categoria="artesanias"
           />
         </div>
       </main>
