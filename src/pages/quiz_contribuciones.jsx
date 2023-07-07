98b00902a0558ee03c244e5fa61329f97e8f0ee8
Hernando Lopez <hernando.rincon@correounivalle.edu.co>
2023-07-06 22:35:48 -0500

    Se corrigen estilos para ser responsive pagina inicial, se corrige estilos de quices y se agrega funcionalidades para aparecer preguntas aleatorias y contar respuestas correctas

--- src/pages/quiz_contribuciones.jsx
+++ src/pages/quiz_contribuciones.jsx
@@ -1,24 +1,21 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Nav_bar_perfil } from "../components/nav-bar-perfil";
-import "../scss/user_profile_style.css";
-import "../scss/boton_toggler_style.css";
-import "../scss/quiz_contribuciones_style.css";
-import "../scss/quices_style.css";
 import { api } from "../api/register_api";
 import { Button, Modal } from "react-bootstrap";
-/**
- * Componente del quiz de contribuciones.
- *
- * Este componente muestra la interfaz del quiz de contribuciones con sus respectivas preguntas.
- */
+import { useNavigate } from "react-router-dom";
+import "../scss/quices_style.css";
 
 export function Quiz_contribuciones() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [data, setData] = useState([]);
   const [preguntas, setPreguntas] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [opcion, setOpcion] = useState("");
+  const [opcionSeleccionada, setOpcionSeleccionada] = useState("");
   const [mensajeVisible, setMensajeVisible] = useState(false);
+  const [correctAnswersCount, setCorrectAnswersCount] = useState(0);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [quizCompleted, setQuizCompleted] = useState(false);
+  const navigate = useNavigate();
 
   /**
    * Alternar la apertura y cierre de la barra lateral.
@@ -31,86 +28,75 @@ export function Quiz_contribuciones() {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
-  /**
-   * Alternar la variable opcion entre "correcta" y "incorrecta".
-   *
-   * Esta función cambia el estado de la variable 'opcion' para controlar
-   * si la opcion seleccionada en una respectiva pregunta es la correcta o no.
-   */
-
-  const selectOption = (esCorrecta) => {
-    if (esCorrecta) {
-      setOpcion("correcta");
-    } else {
-      setOpcion("incorrecta");
+  const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
+    return array;
   };
-
-  /**
-   * Verifica si la opcion que selecciono es la correcta o no.
-   *
-   * Activa un boton para mostrar un mensaje en la pantalla que indica si la respuesta fue correcta o no
-   */
-
-  const verificarRespuesta = () => {
-    setMensajeVisible(true);
-  };
-
-  /**
-   *  Hook de React que permite ejecutar efectos secundarios en componentes funcionales
-   *
-   * En este caso, se está utilizando para llamar a la función fetchData una vez, cuando el
-   * componente se monta por primera vez (debido al arreglo vacío como segundo argumento [])
-   */
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  /**
-   * Realiza una solicitud HTTP para obtener datos desde el endpoint "/china/preguntas_aportes/".
-   *
-   * Hace una llamada a una API para obtener datos desde el endpoint "/china/preguntas_aportes/".
-   * Una vez que se recibe la respuesta exitosa, se llama a la función selectPreguntas con los datos
-   * recibidos y el número 2 como argumentos. En caso de error, muestra un mensaje de error en la consola.
-   */
-
   const fetchData = () => {
     api
       .get("/china/preguntas_aportes/")
       .then((response) => {
-        selectPreguntas(response.data, 2);
-        console.log(data);
+        setData(response.data);
+        const shuffledPreguntas = shuffle(response.data);
+        const selectedPreguntas = shuffledPreguntas.slice(0, 5);
+        setPreguntas(selectedPreguntas);
       })
       .catch((error) => {
         console.error("Error:", error);
       });
   };
 
-  /**
-   * Selecciona un número específico de preguntas al azar de la lista de datos recibidos.
-   *
-   * Selecciona un número específico de preguntas al azar de una lista de datos de preguntas.
-   * La función toma dos parámetros: "data", que representa la lista de datos de preguntas, y "cant",
-   * que indica la cantidad de preguntas a seleccionar. La función actualiza el estado de las preguntas
-   * seleccionadas y garantiza que no se repitan preguntas en la selección. No devuelve ningún valor.
-   */
-
-  const selectPreguntas = (data, cant) => {
-    const listaClonada = data;
-    const elementosAlAzar = [];
-    setData(data);
-    while (elementosAlAzar.length < cant && listaClonada.length > 0) {
-      // Generar un índice aleatorio
-      const indice = Math.floor(Math.random() * listaClonada.length);
-
-      // Agregar el elemento correspondiente al índice aleatorio a la lista de elementos al azar
-      elementosAlAzar.push(listaClonada[indice]);
-      setPreguntas(elementosAlAzar);
-
-      // Eliminar el elemento seleccionado de la lista clonada
-      listaClonada.splice(indice, 1);
+  const selectOption = (esCorrecta, opcionSeleccionada) => {
+    if (esCorrecta) {
+      setOpcion("correcta");
+    } else {
+      setOpcion("incorrecta");
     }
+    setOpcionSeleccionada(opcionSeleccionada);
+  };
+
+  const verificarRespuesta = () => {
+    setMensajeVisible(true);
+
+    if (opcion === "correcta") {
+      setCorrectAnswersCount((prevCount) => prevCount + 1);
+    }
+
+    if (currentQuestionIndex === preguntas.length - 1) {
+      // Última pregunta, mostrar resultados
+      setTimeout(() => {
+        showResults();
+      }, 2000);
+    } else {
+      // Avanzar a la siguiente pregunta
+      setTimeout(() => {
+        setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+        setOpcion("");
+        setOpcionSeleccionada("");
+      }, 2000);
+    }
+  };
+
+  const restartQuiz = () => {
+    setQuizCompleted(false);
+    setPreguntas([]);
+    setCurrentQuestionIndex(0);
+    setCorrectAnswersCount(0);
+    navigate('/profile');
+  };
+
+  const showResults = () => {
+    alert(`Respondiste correctamente ${correctAnswersCount} preguntas.`);
+    setQuizCompleted(true);
+    console.log(correctAnswersCount)
   };
 
   return (
@@ -138,90 +124,131 @@ export function Quiz_contribuciones() {
           <h1 className="titulo-settings">Quiz Contribuciones</h1>
         </div>
 
-        <div className="centered-div">
-          {preguntas.map((item) => (
-            <div key={item.id} className="quiz-options text-center">
-              {/* Pregunta */}
+        {quizCompleted ? (
+          <div className="text-center">
+            <p>
+              Quiz completado. Has respondido correctamente{" "}
+              {correctAnswersCount} preguntas.
+            </p>
+            <button onClick={restartQuiz}>Volver al perfil</button>
+          </div>
+        ) : (
+          <div className="tarjeta-preguntas mt-5">
+            {preguntas.length > 0 &&
+              currentQuestionIndex < preguntas.length && (
+                <>
+                  <div className="tarjeta-header">
+                    <header>
+                      <p className="titulo-pregunta">
+                        {preguntas[currentQuestionIndex].pregunta}
+                      </p>
+                    </header>
+                  </div>
+                  <div className="tarjeta-body">
+                    <div className="radio-container">
+                      <input
+                        type="radio"
+                        className="input-radio"
+                        id="one-a"
+                        name="yes-1"
+                        onChange={() =>
+                          selectOption(
+                            preguntas[currentQuestionIndex].opcion1
+                              .es_respuesta_correcta,
+                            "a"
+                          )
+                        }
+                        checked={opcionSeleccionada === "a"}
+                        required
+                      />
+                      <label
+                        className={`radio ${
+                          opcionSeleccionada === "a" ? "selected" : ""
+                        }`}
+                        htmlFor="one-a"
+                      >
+                        <span className="alphabet">A.</span>{" "}
+                        {preguntas[currentQuestionIndex].opcion1.texto_opcion}
+                      </label>
+                    </div>
+                    <div className="radio-container">
+                      <input
+                        type="radio"
+                        className="input-radio"
+                        id="one-b"
+                        name="yes-1"
+                        onChange={() =>
+                          selectOption(
+                            preguntas[currentQuestionIndex].opcion2
+                              .es_respuesta_correcta,
+                            "b"
+                          )
+                        }
+                        checked={opcionSeleccionada === "b"}
+                      />
+                      <label
+                        className={`radio ${
+                          opcionSeleccionada === "b" ? "selected" : ""
+                        }`}
+                        htmlFor="one-b"
+                      >
+                        <span className="alphabet">B.</span>{" "}
+                        {preguntas[currentQuestionIndex].opcion2.texto_opcion}
+                      </label>
+                    </div>
+                    <div className="radio-container">
+                      <input
+                        type="radio"
+                        className="input-radio"
+                        id="one-c"
+                        name="yes-1"
+                        onChange={() =>
+                          selectOption(
+                            preguntas[currentQuestionIndex].opcion3
+                              .es_respuesta_correcta,
+                            "c"
+                          )
+                        }
+                        checked={opcionSeleccionada === "c"}
+                      />
+                      <label
+                        className={`radio ${
+                          opcionSeleccionada === "c" ? "selected" : ""
+                        }`}
+                        htmlFor="one-c"
+                      >
+                        <span className="alphabet">C.</span>{" "}
+                        {preguntas[currentQuestionIndex].opcion3.texto_opcion}
+                      </label>
+                    </div>
+                  </div>
 
-              <header>
-                <p className="centered-div">{item.pregunta}</p>
-              </header>
-
-              {/* Respuestas */}
-
-              {/* Opcion 1 */}
-              <div>
-                <input
-                  type="radio"
-                  className="input-radio"
-                  id="one-a"
-                  name="yes-1"
-                  onChange={() => {
-                    selectOption(item.opcion1.es_respuesta_correcta);
-                  }}
-                  required
-                />
-                <label className="radio-label" for="one-a">
-                  <span className="alphabet">A</span>{" "}
-                  {item.opcion1.texto_opcion}
-                </label>
-
-                {/* Opcion 2 */}
-
-                <input
-                  type="radio"
-                  className="input-radio"
-                  id="one-b"
-                  name="yes-1"
-                  onChange={() => {
-                    selectOption(item.opcion2.es_respuesta_correcta);
-                  }}
-                />
-                <label className="radio-label" for="one-b">
-                  <span className="alphabet">B</span>{" "}
-                  {item.opcion2.texto_opcion}
-                </label>
-
-                {/* Opcion 3 */}
-
-                <input
-                  type="radio"
-                  className="input-radio"
-                  id="one-c"
-                  name="yes-1"
-                  onChange={() => {
-                    selectOption(item.opcion3.es_respuesta_correcta);
-                  }}
-                />
-                <label className="radio-label" for="one-c">
-                  <span className="alphabet">C</span>{" "}
-                  {item.opcion3.texto_opcion}
-                </label>
-              </div>
-
-              {/* Verificar respuesta */}
-
-              <a id="btn" type="submit" onClick={verificarRespuesta}>
-                Verificar
-              </a>
-            </div>
-          ))}
-
-          <Modal show={mensajeVisible} onHide={() => setMensajeVisible(false)}>
-            <Modal.Header closeButton>
-              <Modal.Title>Tu respuesta</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>Tu respuesta es {opcion}</Modal.Body>
-            <Modal.Footer>
-              <Button
-                variant="secondary"
-                onClick={() => setMensajeVisible(false)}
-              >
-                Cerrar
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </div>
+                  <a
+                    className="btn-verificar mt-3"
+                    id="btn"
+                    type="submit"
+                    onClick={verificarRespuesta}
+                  >
+                    Verificar
+                  </a>
+                </>
+              )}
+          </div>
+        )}
+        <Modal show={mensajeVisible} onHide={() => setMensajeVisible(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Tu respuesta</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Tu respuesta es {opcion} </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="secondary"
+              onClick={() => setMensajeVisible(false)}
+            >
+              Cerrar
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </main>
     </>
   );
